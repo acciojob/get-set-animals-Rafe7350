337acaa6c76595be4ef30744c4eb446e6d437164
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-20 12:55:56 +0530

    add

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //complete this code
-class Animal {}
-
-class Dog extends Animal {}
-
-class Cat extends Animal {}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Animal {
+    constructor(species) {
+      this._species = species;
+    }
+  
+    get species() {
+      return this._species;
+    }
+  
+    makeSound() {
+      console.log(`The ${this._species} makes a sound`);
+    }
+  }
+  
+  class Cat extends Animal {
+    purr() {
+      console.log(`purr`);
+    }
+  }
+  
+  class Dog extends Animal {
+    bark() {
+      console.log(`woof`);
+    }
+  }
+  
+  // Do not change the code below this line
+  window.Animal = Animal;
+  window.Dog = Dog;
+  window.cat = Cat;
